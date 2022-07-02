3bc60e343f26d6982489ca057add817de6c5d479
ElfGod <albertogx@icloud.com>
2022-07-01 22:28:25 -0500

    Adding Form Inputs

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -7,6 +7,7 @@ import './ExpenseItem.css'
 function ExpenseItem(props) {
   // let title = props.title
   const [title, setTitle] = useState(props.title)
+  console.log('ExpenseItem evaluated by React')
 
   const clickHandler = () => {
     // title = 'Updated!'
@@ -18,7 +19,7 @@ function ExpenseItem(props) {
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
